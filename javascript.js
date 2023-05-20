d7b2b57d1f8c23d562caf5b54545c2296973367b
dustvans <dustinevans07@gmail.com>
2023-05-19 21:23:01 -0500

    Added functionality to menu when screen size shrinks

--- javascript.js
+++ javascript.js
@@ -1,13 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var mobileCta = document.getElementById('mobile-cta');
+    var mobileExit = document.getElementById('mobile-exit');
+    var nav = document.querySelector('.navbar nav');
+
+    mobileCta.addEventListener('click', function() {
+        nav.classList.add('active');
+        mobileCta.style.display = 'none';
+        mobileExit.style.display = 'block';
+    });
+
+    mobileExit.addEventListener('click', function() {
+        nav.classList.remove('active');
+        mobileCta.style.display = 'block';
+        mobileExit.style.display = 'none';
+    });
+});
 
 
 
 const inputs = document.querySelectorAll('input')
-
 const patterns = {
     telephone:/^\d{7,11}$/,
     email: /^([a-z\d\.-]+)@([a-z\d-]+).([a-z]{2,8})(\.[a-z]{2,8})?$/i
 };
-
 //validation function
 function validate(field, regex) {
     if (regex.test(field.value)) {
@@ -17,7 +32,6 @@ function validate(field, regex) {
         field.className = 'invalid';
     }
 }
-
 //for each loop for input fields
 inputs.forEach((input) => {
     input.addEventListener('keyup', (e) => {
